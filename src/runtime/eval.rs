144b58f5a1b590728708bcffbb908b5de8d9bbed
chickenbreeder <chickenbreeder@users.noreply.github.com>
2023-09-13 02:20:26 +0200

    Create an environment for every function call, evaluate expressions/statements within that env

--- src/runtime/eval.rs
+++ src/runtime/eval.rs
@@ -10,14 +10,12 @@ use crate::{
 };
 
 pub(crate) struct Interpreter<'src> {
-    global_env: Environment<'src>,
     functions: HashMap<&'src str, FnDecl<'src>>,
 }
 
 impl<'src> Interpreter<'src> {
     pub fn new() -> Self {
         Self {
-            global_env: Environment::new(),
             functions: HashMap::new(),
         }
     }
@@ -26,78 +24,118 @@ impl<'src> Interpreter<'src> {
         let mut parser = Parser::new(src);
 
         while let Some(stmt) = parser.parse_stmt()? {
-            match *stmt {
-                Statement::VarDecl { id, value } => {
-                    let value = self.eval_expr(&value)?;
-                    println!("DECL: {id} = {value}");
-
-                    if self.global_env.contains_key(id) {
-                        return Err(ErrorKind::RuntimeError(format!(
-                            "Variable `{id}` has already been declared"
-                        )));
-                    } else {
-                        self.global_env.insert(id, value);
-                    }
-                }
-                Statement::Assignment { id, value } => {
-                    let value = self.eval_expr(&value)?;
-
-                    if let Some(v) = self.global_env.get_mut(id) {
-                        *v = value;
-                    } else {
-                        return Err(ErrorKind::RuntimeError(format!(
-                            "Undefined reference `{id}`"
-                        )));
-                    }
-                }
-                Statement::FnDecl(decl) => {
-                    println!("FN DECL: {} = {:?}", decl.id, &decl.body);
-                    self.functions.insert(decl.id, decl);
-                }
-                _ => (),
-            }
+            self.eval_top_level_stmt(stmt)?;
         }
 
+        let _ = self.eval_fn_call("main", &[])?;
         Ok(())
     }
 
-    pub fn eval_expr_str(&self, expr: &'static str) -> PResult<Option<i64>> {
+    #[allow(dead_code)]
+    pub fn eval_expr_str(&mut self, expr: &'static str) -> PResult<Option<i64>> {
         let mut parser = Parser::new(expr);
+        let mut global_env = Environment::new();
 
         match parser.parse_expr()? {
-            Some(expr) => Ok(Some(self.eval_expr(&expr)?)),
+            Some(expr) => Ok(Some(self.eval_expr_in_env(&expr, &mut global_env)?)),
             None => Ok(None),
         }
     }
 
-    fn eval_expr(&self, expr: &Expression) -> PResult<i64> {
+    fn eval_top_level_stmt(&mut self, stmt: Box<Statement<'src>>) -> PResult<()> {
+        match *stmt {
+            Statement::FnDecl(decl) => {
+                self.functions.insert(decl.id, decl);
+            }
+            other => {
+                return Err(ErrorKind::RuntimeError(format!(
+                    "Expected top level statement, found {other:?}"
+                )))
+            }
+        }
+        Ok(())
+    }
+
+    fn eval_stmt_in_env(&self, stmt: &Statement<'src>, env: &mut Environment<'src>) -> PResult<()> {
+        match stmt {
+            Statement::VarDecl { id, value } => {
+                let value = self.eval_expr_in_env(&value, env)?;
+                println!("DECL: {id} = {value}");
+
+                if env.contains_key(id) {
+                    return Err(ErrorKind::RuntimeError(format!(
+                        "Variable `{id}` has already been declared"
+                    )));
+                } else {
+                    env.insert(id, value);
+                }
+            }
+            Statement::Assignment { id, value } => {
+                let value = self.eval_expr_in_env(&value, env)?;
+
+                if let Some(v) = env.get_mut(id) {
+                    *v = value;
+                } else {
+                    return Err(ErrorKind::RuntimeError(format!(
+                        "Undefined reference `{id}`"
+                    )));
+                }
+            }
+            other => {
+                return Err(ErrorKind::RuntimeError(format!(
+                    "Expected statement, found {other:?}"
+                )))
+            }
+        }
+        Ok(())
+    }
+
+    fn eval_expr_in_env(&self, expr: &Expression, env: &mut Environment<'src>) -> PResult<i64> {
         match expr {
             Expression::Number(v) => Ok(*v),
-            Expression::Grouping(expr) => self.eval_expr(expr),
-            Expression::Unary(expr) => Ok(-self.eval_expr(expr)?),
-            Expression::Binary { lhs, op, rhs } => match op {
-                Operator::Plus => Ok(self.eval_expr(lhs)? + self.eval_expr(rhs)?),
-                Operator::Minus => Ok(self.eval_expr(lhs)? - self.eval_expr(rhs)?),
-                Operator::Mul => Ok(self.eval_expr(lhs)? * self.eval_expr(rhs)?),
-            },
+            Expression::Grouping(expr) => self.eval_expr_in_env(expr, env),
+            Expression::Unary(expr) => Ok(-self.eval_expr_in_env(expr, env)?),
+            Expression::Binary { lhs, op, rhs } => {
+                let lhs = self.eval_expr_in_env(lhs, env)?;
+                let rhs = self.eval_expr_in_env(rhs, env)?;
+
+                match op {
+                    Operator::Plus => Ok(lhs + rhs),
+                    Operator::Minus => Ok(lhs - rhs),
+                    Operator::Mul => Ok(lhs * rhs),
+                }
+            }
             Expression::VarRef(id) => {
-                if let Some(value) = self.global_env.get(id) {
+                if let Some(value) = env.get(id) {
                     return Ok(*value);
                 }
                 Err(ErrorKind::RuntimeError(format!(
                     "Undefined reference `{id}`"
                 )))
             }
-            Expression::FnCall { id, params: _ } => {
-                let decl = self.functions.get(id).unwrap();
-                let statements = &decl.body[..decl.body.len() - 1];
+            Expression::FnCall { id, params } => self.eval_fn_call(id, &params),
+        }
+    }
 
-                match decl.body.last() {
-                    Some(Statement::Return(expr)) => Ok(self.eval_expr(expr)?),
-                    _ => unreachable!("Last statement in function body was not a return statement. This should never happen")
-                }
+    fn eval_fn_call(&self, id: &'src str, _params: &[Expression<'src>]) -> PResult<i64> {
+        let decl = match self.functions.get(id) {
+            Some(decl) => decl,
+            None => {
+                return Err(ErrorKind::RuntimeError(format!(
+                    "Undefined function `{id}`"
+                )))
             }
-            _ => todo!(),
+        };
+        let statements = &decl.body[..decl.body.len() - 1];
+        let mut env = Environment::new();
+
+        for stmt in statements {
+            self.eval_stmt_in_env(stmt, &mut env)?;
+        }
+
+        match decl.body.last() {
+            Some(Statement::Return(expr)) => Ok(self.eval_expr_in_env(expr, &mut env)?),
+            _ => unreachable!("Last statement in function body was not a return statement. This should never happen")
         }
     }
 }
